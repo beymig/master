5b641c17292707fdc308efa524d13b5db12ca0c2
beymig <beymig@gmail.com>
2016-03-01 01:02:49 -0800

    added missille and score and lives labels

---
+++ Assets/_Scripts/SpawnMisille.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnMisille : MonoBehaviour {
+
+    public Transform[] misilleSpawns;
+    public GameObject misille;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < misilleSpawns.Length; i++)
+        {
+            int misilleFlip = Random.Range(0, 10);
+            if (misilleFlip > 0)
+                Instantiate(misille, misilleSpawns[i].position, Quaternion.identity);
+        }
+    }
+}
